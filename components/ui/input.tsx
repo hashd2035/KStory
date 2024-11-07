e997e4f76a87554cd272eec95696e5d976b7cd89
zhac218 <hashd2035@gmail.com>
2024-11-07 08:57:19 +0000

    Simplify next config and remove unused dependencies

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,17 +1,17 @@
 "use client"
 
 import * as React from "react"
-import { cn } from "@/lib/utils"
 
-const Input = React.forwardRef<HTMLInputElement, React.InputHTMLAttributes<HTMLInputElement>>(
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  className?: string
+}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ className, type, ...props }, ref) => {
     return (
       <input
         type={type}
-        className={cn(
-          "flex h-10 w-full rounded-md border border-white/10 bg-transparent px-3 py-2 text-sm text-white ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-white/50 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50",
-          className
-        )}
+        className={className}
         ref={ref}
         {...props}
       />
